061860be8bddebdfc7c4bdb5086a910911503947
gpbio10 <gradypalfrey@gmail.com>
2021-07-19 23:39:32 -0700

    Basic arima model

---
+++ main.py
@@ -0,0 +1,48 @@
+from clr import AddReference
+
+AddReference("System")
+AddReference("QuantConnect.Algorithm")
+AddReference("QuantConnect.Common")
+
+from System import *
+from QuantConnect import *
+from QuantConnect.Algorithm import *
+
+
+class Algorithm(QCAlgorithm):
+    def Initialize(self):
+        self.SetStartDate(2021, 1, 1)
+        self.SetEndDate(2021, 7, 12)
+
+        self.EnableAutomaticIndicatorWarmUp = True
+
+        self.portfolio = ["DAL", "AC", "AAL"]
+        for stock in self.portfolio:
+            self.AddEquity(stock, Resolution.Hour)
+
+    def OnData(self, data):
+        '''OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
+
+        Arguments:
+            data: Slice object keyed by symbol containing the stock data
+        '''
+        self.arimax()
+
+    def arimax(self):
+        for stock in self.portfolio:
+            self.arima = self.ARIMA(stock, 10, 1, 10, 50)
+            self.ar = self.ARIMA(stock, 1, 1, 0, 50)
+            self.last = self.ar.Current.Value
+
+            if self.arima.IsReady:
+                if abs(self.arima.Current.Value - self.ar.Current.Value) > 1:
+                    self.Debug("Arima Current: " + str(self.arima.Current.Value) + " Last: " + str(self.last))
+                    difference = self.arima.Current.Value - self.last
+                    if self.arima.Current.Value > self.last:
+                        # self.Debug("Purchasing " + str(stock))
+                        if (abs(difference) > 10):
+                            self.SetHoldings(stock, 0.33)
+                        else:
+                            self.SetHoldings(stock, difference * 0.033)
+                    self.Debug(difference)
+                self.last = self.arima.Current.Value
