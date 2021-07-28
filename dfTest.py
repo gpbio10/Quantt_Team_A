60f8479e7234e31c75a73fda8e43f79ad349903f
gpbio10 <gradypalfrey@gmail.com>
2021-07-27 23:22:50 -0700

    algo progression

--- dfTest.py
+++ dfTest.py
@@ -1,20 +1,45 @@
 
 import pandas as pd
 import numpy as np
-import datetime
+from datetime import datetime, timedelta
 
-source = "https://www.dropbox.com/s/nuoi78pkna9956o/throughputTest.csv?dl=1"
-header_list = ["Date", "Throughput"]
-df = pd.read_csv(source, names=header_list)
-# df['Date'] = df['Date'].astype('datetime64[ns]')
+def convert_to_datetime(string_date):
+    return datetime.strptime(string_date, '%m/%d/%Y')
 
-for index, row in df.iterrows():
-    date_time_obj = datetime.datetime.strptime(row['Date'], '%m/%d/%Y')
-    row['Date'] = date_time_obj
-    row['Throughput'] = float(row['Throughput'].replace(',', ''))
-#
-df['Log returns'] = np.log(df['Throughput']/df['Throughput'].shift())
+def toInt(throughputString):
+    return int(throughputString)
 
-df['Log returns'].std()
+def main():
+    source = "https://www.dropbox.com/s/1wo4t5ydkrzderr/throughput.csv?dl=1"
+    header_list = ["Date", "Throughput"]
+    df = pd.read_csv(source, names=header_list)
 
-print(df.head())
+    # for index, row in df.iterrows():
+    #     df['Date'] = datetime.strptime(df['Date'], '%m/%d/%Y')
+
+    df["Date"] = df["Date"].apply(convert_to_datetime)
+    df['Throughput'] = df["Throughput"].apply(toInt)
+    df['Log returns'] = np.log(df['Throughput']/df['Throughput'].shift())
+
+    df['Log returns'].std()
+
+
+    x = datetime(2021, 7, 26)
+    x = datetime(x.year, x.month, x.day, 0, 0, 0)
+    x = x - timedelta(days=1)
+    print(x)
+
+    index = df.index
+    condition = df["Date"] == x
+    startDate = index[condition]
+    indices = startDate.tolist()
+    print(indices)
+
+
+    volatility = df['Log returns'][indices[0]]
+    print(volatility)
+
+    print(df)
+
+
+main()
--- main.py
+++ main.py
@@ -68,3 +68,115 @@ class Algorithm(QCAlgorithm):
         df = pd.read_csv(StringIO(source), names=header_list)
         df['Date'] = df['Date'].astype('datetime64[ns]')
 
+#####--------------------------------------------
+from clr import AddReference
+
+AddReference("System")
+AddReference("QuantConnect.Algorithm")
+AddReference("QuantConnect.Common")
+
+from System import *
+from QuantConnect import *
+from QuantConnect.Algorithm import *
+
+import pandas as pd
+import numpy as np
+from io import StringIO
+
+from statsmodels.tsa.stattools import adfuller
+
+from datetime import datetime, timedelta
+
+volatility = 0.1
+
+
+class Algorithm(QCAlgorithm):
+    def Initialize(self):
+        self.SetStartDate(2021, 1, 1)
+        self.SetEndDate(2021, 7, 12)
+
+        self.EnableAutomaticIndicatorWarmUp = True
+
+        self.portfolio = ["DAL", "AC", "AAL"]
+        for stock in self.portfolio:
+            self.AddEquity(stock, Resolution.Hour)
+
+        # self.GetSource()
+
+        self.Schedule.On(self.DateRules.EveryDay("DAL"), self.TimeRules.AfterMarketOpen("DAL", 210), self.GetSource)
+
+        # self.Schedule.On(self.DateRules.EveryDay("DAL"), self.TimeRules.AfterMarketOpen("DAL", 220), self.Rebalance)
+
+    def OnData(self, data):
+        '''OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
+
+        Arguments:
+            data: Slice object keyed by symbol containing the stock data
+        '''
+        self.arimax()
+
+    def arimax(self):
+        for stock in self.portfolio:
+
+            # closing_prices = self.History(stock, 20, Resolution.Hour).values
+
+            self.arima = self.ARIMA(stock, 10, 1, 10, 50)
+            self.ar = self.ARIMA(stock, 1, 1, 0, 50)
+            self.last = self.ar.Current.Value
+
+            if self.arima.IsReady:
+                if abs(self.arima.Current.Value - self.ar.Current.Value) > 1:
+                    self.Debug("Arima Current: " + str(self.arima.Current.Value) + " Last: " + str(self.last))
+                    difference = self.arima.Current.Value - self.last
+
+                    self.Debug("Buying Power = " + self.Portfolio.GetBuyingPower(stock, self.OrderDirection.Buy));
+
+                    if (abs(difference) > 10):
+                        availableHoldings = (1 - (
+                                    self.Portfolio.TotalHoldingsValue / self.Portfolio.TotalPortfolioValue)) / self.NUM_POSITIONS
+
+                        if availableHoldings < 0:
+                            availableHoldings = 0
+
+                        self.Debug("Buying: " + str(stock));
+                        self.SetHoldings(stock, difference * -(0.1 * volatility))
+
+                    self.Debug(difference)
+                self.last = self.arima.Current.Value
+
+    def convert_to_datetime(self, string_date):
+        return datetime.strptime(string_date, '%m/%d/%Y')
+
+    def toInt(self, throughputString):
+        return int(throughputString)
+
+    def GetSource(self):
+        source = self.Download("https://www.dropbox.com/s/1wo4t5ydkrzderr/throughput.csv?dl=1")
+        header_list = ["Date", "Throughput"]
+        df = pd.read_csv(StringIO(source), names=header_list)
+        df["Date"] = df["Date"].apply(self.convert_to_datetime)
+        self.Debug("Passed convert to datetime")
+        df["Throughput"] = df["Throughput"].apply(self.toInt)
+        self.Debug("Passed convert to int")
+        df['Log returns'] = np.log(df['Throughput'] / df['Throughput'].shift())
+        self.Debug("Passed log returns")
+        df['Log returns'].std()
+        self.Debug("Passed std")
+
+        currentDate = self.Time
+        currentDate = datetime(currentDate.year, currentDate.month, currentDate.day, 0, 0, 0)
+        self.Debug(currentDate)
+        currentDate = currentDate - timedelta(days=1)
+
+        self.Debug("Passed subtract date")
+        index = df.index
+        condition = df["Date"] == currentDate
+        startDate = index[condition]
+        indices = startDate.tolist()
+
+        volatility = df['Log returns'][indices[0]]
+
+        self.Debug(volatility)
+
+
+
